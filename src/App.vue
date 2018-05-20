ffe7db27c42013b990c40afd38b85fbb933ec5b8
System Administrator <root@Audus-MacBook-Pro.local>
2018-05-20 10:21:40 +0100

    minor mods

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,33 @@
 <template>
+  <div id="app">
+
+
+    <!-- Route Outlet -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  // ...
+};
+</script>
+
+<template>
   <div>
     <nav>
       <div class="container">
         <ul class="nav__left">
+
+          <!-- Navigation Links -->
           <li><router-link to="/">Home</router-link></li>
           <li><router-link to="/admin">Admin</router-link></li>
           <li><router-link to="/cart">Cart</router-link></li>
+
         </ul>
       </div>
     </nav>
+
     <router-view></router-view>
   </div>
 </template>
