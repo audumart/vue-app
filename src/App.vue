b861c4f21ead994e78148921ad394eb990a4f0a1
System Administrator <root@Audus-MacBook-Pro.local>
2018-05-20 01:13:56 +0100

    linking pages

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,14 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
-</template>
+  <div>
+    <nav>
+      <div class="container">
+        <ul class="nav__left">
 
-<script>
-export default {
-  name: 'App',
-};
-</script>
+          
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link to="/admin">Admin</router-link></li>
+          <li><router-link to="/cart">Cart</router-link></li>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+        </ul>
+      </div>
+    </nav>
